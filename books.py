7820af4f3a200440f1c9f744de3693eef7ae0535
YazRas <96800707+YazRas@users.noreply.github.com>
2023-08-15 01:07:15 +0000

    Added a sorting algorithm from my previous project.

--- books.py
+++ books.py
@@ -2,3 +2,16 @@ import utils
 import sorts
 
 bookshelf = utils.load_books('books_small.csv')
+for book in bookshelf:
+  print(book['title'])
+print(ord("a"))
+print(ord(" "))
+print(ord("A"))
+ 
+def by_title_ascending(book_a, book_b):
+  return book_a['title_lower'] > book_b['title_lower']
+  sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
+  for book in sort_1:
+    print(book['title'])
+def by_author_ascending(book_a, book_b):
+  return book_a['author_lower'] > book_b['author_lower']
